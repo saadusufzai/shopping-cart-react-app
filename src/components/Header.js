2b439d06f21b807c7fc0c0a71e7273a02cb674cb
Saad Aslam <saadusufzai@gmail.com>
2020-07-01 03:41:28 +0500

    UI complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { AppBar, Toolbar, Link, Button } from '@material-ui/core'
+import { AppBar,Badge, Toolbar, IconButton, Link, Button } from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles/'
-
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import LocalMallIcon from '@material-ui/icons/LocalMall';
 const useStyle = makeStyles((theme)=>({
     root:{
       backgroundColor:'#282828'
+    },
+    grow:{
+        width:'100%',
+        textAlign:'right',
     }
 }))
 
@@ -16,9 +21,23 @@ export default function Header() {
             <Toolbar>
                <Button color='secondary'>  <Link to='#' color='#fff' >HOME</Link> </Button>
                <Button color='secondary'>  <Link to='#' color='#fff'>Contact</Link> </Button>
-               <Button color='secondary'>  <Link to='#' color='#fff'>Wish List</Link></Button>
+               <Button color='secondary'>  <Link noWrap to='#' color='#fff'>Wish List</Link></Button>
                <Button color='secondary'>  <Link to='#' color='#fff'>Cart</Link></Button>
+            
+              <div className={classes.grow}>
+              < IconButton aria-label="show 4 new mails" color="inherit">
+                <Badge badgeContent={4} color="secondary">
+                    <ShoppingCartIcon />
+                </Badge>
+                </IconButton>
+                <IconButton aria-label="show 17 new notifications" color="inherit">
+                <Badge badgeContent={17} color="secondary">
+                    <LocalMallIcon />
+                </Badge>
+                </IconButton>
+              </div>
             </Toolbar>
+
         </AppBar>
     )
 }
