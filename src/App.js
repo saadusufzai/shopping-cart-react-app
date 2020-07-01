e13145d56491e705579b23242e33383fe428ca76
Saad Aslam <saadusufzai@gmail.com>
2020-07-01 06:55:25 +0500

    table added in cart page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home';
 import Header from './components/Header';
+import Cart from './components/cart'
 
 function App() {
   return (
    <>
       <Header />
-      <Home/ >
+      <Cart/>
    </>
   );
 }
