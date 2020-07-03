164a70fbbceefac7ab3695f4c71191ac6026611f
Saad Aslam <saadusufzai@gmail.com>
2020-07-03 12:32:23 +0500

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "./App.css";
 import Home from "./components/Home";
 import Header from "./components/Header";
-
+import Repo from './components/Repo';
 import Cart from "./components/Cart/cart";
 import { GlobalProvider } from "./components/Context/GlobalContext";
 
@@ -16,13 +16,14 @@ function App() {
               <Routes>
                 <Route path='/' element={<Home />}></Route>
                 <Route path='cart' element={<Cart />}></Route>
+                <Route path='repo' element={<Repo />}></Route>
               </Routes>
           </Router>  
         </GlobalProvider>
           
+         
         
      
   );
-}
-
+} 
 export default App;
---
+++ src/components/Repo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "@material-ui/core";
+
+const Repo = () => {
+  return (
+    <div className='repo'>
+      <h1> MUHAMMAD SAAD ASLAM</h1>
+      <Link  to='https://github.com/saadusufzai/shopping-cart-react-app' >GIT HUB REPOSITORY </Link>
+    </div>
+  );
+};
+
+export default Repo;
